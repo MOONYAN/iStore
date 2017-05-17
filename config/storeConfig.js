77f2917329ecba831c2f5d0bc6f0db0bd7b0bcb6
Owen <1787hz@gmail.com>
2017-05-17 15:23:45 +0800

    FCM

--- config/storeConfig.js
+++ config/storeConfig.js
@@ -1,9 +1,16 @@
 var storeId = '24';
 
 module.exports = {
-    storeId: storeId,
-    storeName: `Store ${storeId}`,
-    storeDB: `mongodb://Apps${storeId}:a1234@104.199.219.156:27017/Apps${storeId}`,
-    storeSecret: `store${storeId}`,
-    storePath: `/apps${storeId}/istore`
+    istore: {
+        fcmServerKey: 'AIzaSyCa_MXHiw6SS9aqYYJ_VXmTm-_xNRFdl9g'
+    },
+
+    store: {
+        storeId: storeId,
+        storeName: `Store ${storeId}`,
+        storeDB: `mongodb://Apps${storeId}:a1234@104.199.219.156:27017/Apps${storeId}`,
+        storeSecret: `store${storeId}`,
+        storePath: `/apps${storeId}/istore`,
+        storeTopic: `/topics/store${storeId}`
+    }
 };
